b88ac93296742f3ae89ca2bcf57efb3c12a4bfa4
SergeyDushkin <leirbythe@gmail.com>
2016-12-10 19:41:05 +0300

    @

--- src/app/clients/address/address-list/address-list.component.ts
+++ src/app/clients/address/address-list/address-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Address } from '../address';
 
 @Component({
   selector: 'app-address-list',
@@ -6,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddressListComponent implements OnInit {
 
-  constructor() { }
+  private address : Address[];
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.address = this.route.snapshot.data['address'];
   }
 
 }
