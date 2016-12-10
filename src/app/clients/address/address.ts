3b3154c197177b9596009b29442214ab841fb13f
SergeyDushkin <leirbythe@gmail.com>
2016-12-10 19:38:16 +0300

    @

--- src/app/clients/address/address.ts
+++ src/app/clients/address/address.ts
@@ -1,2 +1,9 @@
 export class Address {
+    public id : string;
+    public name : string;
+
+    public constructor(data:any = {}) {
+        this.id = data.id || "";
+        this.name = data.name || "";
+    }
 }
