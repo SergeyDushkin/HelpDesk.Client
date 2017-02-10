636da815826143675246aeadeb940dd098d8083b
SergeyDushkin <leirbythe@gmail.com>
2017-02-10 10:41:12 +0300

    SmtpSettingsComponent

--- src/app/settings/smtp.settings.component.ts
+++ src/app/settings/smtp.settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 
 import { SmtpSettings } from './smtp.settings';
@@ -14,7 +15,7 @@ export class SmtpSettingsComponent implements OnInit {
   
   SmtpSettings: SmtpSettings;
   
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
     //this.SmtpSettings = this.route.snapshot.data['tickets'];
