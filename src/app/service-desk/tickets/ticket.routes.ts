62550746a085ace641e87deafb7794dc7b368a0c
SergeyDushkin <leirbythe@gmail.com>
2017-02-10 17:10:22 +0300

    add resolve for new ticket

--- src/app/service-desk/tickets/ticket.routes.ts
+++ src/app/service-desk/tickets/ticket.routes.ts
@@ -9,9 +9,13 @@ import { TicketNewComponent } from './ticket-new/ticket-new.component';
 import { TicketListResolve } from './ticket-list-resolve.service';
 import { TicketDetailResolve } from './ticket-detail-resolve.service';
 
+import { PriorityListResolve } from '../ticket-priority/priority-list-resolve.service';
+import { StatusListResolve } from '../ticket-status/status-list-resolve.service';
+import { ClientListResolve } from '../clients/client-list-resolve.service';
+
 const routes: Routes = [
   { path: 'tickets', component: TicketListComponent, resolve: { tickets: TicketListResolve } },
-  { path: 'tickets/create', component: TicketNewComponent },
+  { path: 'tickets/create', component: TicketNewComponent, resolve: { 'ticket-priority': PriorityListResolve, 'ticket-status': StatusListResolve, 'client': ClientListResolve } },
   { path: 'tickets/:id', component: TicketDetailComponent, resolve: { ticket: TicketDetailResolve } }
 ];
 
