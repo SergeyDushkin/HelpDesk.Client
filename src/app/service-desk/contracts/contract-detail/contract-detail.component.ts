1f3c43410e9b717ef28066f11524bc5ba0c7d511
SergeyDushkin <leirbythe@gmail.com>
2017-02-10 18:49:18 +0300

    contract date

--- src/app/service-desk/contracts/contract-detail/contract-detail.component.ts
+++ src/app/service-desk/contracts/contract-detail/contract-detail.component.ts
@@ -34,4 +34,34 @@ export class ContractDetailComponent implements OnInit {
       .then(() => this.location.back());
   }
 
+  set contractDate(e: any) {
+    e = e.split('-');
+    let d = new Date(Date.UTC(e[0], e[1]-1, e[2]));
+    this.data.date.setFullYear(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate());
+  }
+
+  get contractDate() {
+    return this.data.date.toISOString().substring(0, 10);
+  }
+
+  set contractStartDate(e: any) {
+    e = e.split('-');
+    let d = new Date(Date.UTC(e[0], e[1]-1, e[2]));
+    this.data.startDate.setFullYear(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate());
+  }
+
+  get contractStartDate() {
+    return this.data.startDate.toISOString().substring(0, 10);
+  }
+
+  set contractEndDate(e: any) {
+    e = e.split('-');
+    let d = new Date(Date.UTC(e[0], e[1]-1, e[2]));
+    this.data.endDate.setFullYear(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate());
+  }
+
+  get contractEndDate() {
+    return this.data.endDate.toISOString().substring(0, 10);
+  }
+
 }
